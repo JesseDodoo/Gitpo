55a7cbf8847258161afd763d7e5d31e3928ba54c
jesse dodoo <jesse.dodoo@gmail.com>
2022-04-13 09:58:46 +0100

    fixed error messages on routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import * as Pages from './pages'
-import {Navbar} from './components'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import * as Pages from "./pages";
+import { Navbar } from "./components";
 
-function App () {
-    return(
+function App() {
+  return (
     <>
-        <Navbar />
-        {/* <Routes>
-            <Route path="/" element={<Pages.Home />} />
-            <Route path="/about" element={<Pages.About />} />
-            <Route path="/search" element={<Pages.Search />} />
-
-        </Routes> */}
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Pages.Home />} />
+          <Route path="/about" element={<Pages.About />} />
+          <Route path="/search" element={<Pages.Search />} />
+        </Routes>
+      </main>
     </>
-
-    )
+  );
 }
 
 export default App;
-
