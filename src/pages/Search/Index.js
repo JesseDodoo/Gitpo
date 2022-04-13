885ad7eaf1dd85a0eb7487703c31833a896f5952
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-13 10:45:41 +0100

    added functionality to search

--- src/pages/Search/Index.js
+++ src/pages/Search/Index.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import {useNavigate as Navigate} from 'react-router-dom';
+import axios from 'axios';
 
 export default function Search() {
-  return (
-    <div>Search</div>
-  )
+
+
+
+  const [SearchQuery, SetSearchQuery] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [userOutput, setUserOutput] = useState([]);
+
+  console.log("what we need", SearchQuery)
+
+  async function getRepos(){
+    if(SearchQuery !== ""){
+      try{
+        const {data} = await axios.get(`https://api.github.com/users/${SearchQuery}/repos`)
+        console.log(data)
+        setUserOutput(data);
+      }
+      catch(err){
+        alert(err)
+      }
+  
+
+    }
+  }
+
+  useEffect(() => {
+    getRepos();
+
+  }, [SearchQuery])
+
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    SetSearchQuery(userInput);
+    setUserInput("");
+};
+
+const SetSearchValue = e => {
+  setUserInput(e.target.value)
+};
+
+function renderRepos () {
+  return userOutput.map(output => <><a href={output.html_url}>{output.name},{output.forks_count}</a></>)
+
+}
+
+
+  return (<>
+    <form onSubmit={handleFormSubmit}>
+    <input type='text' placeholder='SearchRepos' value={userInput} onChange={SetSearchValue} />
+    <input type="submit" value="Submit" />
+</form>
+{renderRepos()}
+  </>)
 }
 
